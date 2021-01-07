a6cc3494c54b7fbca906992b5ca013510d73d224
SG1701 <77087504+SG1701@users.noreply.github.com>
2021-01-06 22:27:39 -0600

    Update LearnMore.tsx

--- components/pages/learn-more/LearnMore.tsx
+++ components/pages/learn-more/LearnMore.tsx
@@ -6,8 +6,8 @@ function LearnMore() {
     <Wrapper id={"learn-more"}>
       <Content>
         <Product title={"Post Consumer Recycled PET"} description={"rPET flakes & Pellets"} image={"/img/learn-more/product.jpg"} />
-        <Product title={"Other Polymers"} description={"Recycled PP, LDPE"} image={"/img/learn-more/product.jpg"} />
-        <Product title={"Services"} description={""} image={"/img/learn-more/product.jpg"} />
+        <Product title={"Other Polymers"} description={"Recycled PP, LDPE"} image={"/img/learn-more/other-polymers.jpg"} />
+        <Product title={"Services"} description={""} image={"/img/learn-more/services.gif"} />
       </Content>
       <Wave2 src="/img/learn-more/wave2.svg" alt="" />
     </Wrapper>
